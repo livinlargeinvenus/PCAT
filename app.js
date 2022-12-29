936f589ddeb6b7c649b0e7e85784bf933538fbec
Onur Alp <onuralpkayag@gmail.com>
2022-12-29 22:06:23 +0300

    Static file uploaded

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-    console.log('function working');
-}; 
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// MIDDLEWARES
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './temp/index.html'));
+});
+
+// port
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda baslatildi...`);
+});
