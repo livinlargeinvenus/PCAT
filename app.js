f409487fe5548de59459a16a328c25a65f019fb8
Onur Alp <onuralpkayag@gmail.com>
2023-01-05 16:54:20 +0300

    MVC | photo detail update and deletion

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 
 const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
 
 const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -25,49 +28,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+    methodOverride('_method', {
+        methods: ['POST', 'GET'],
+    })
+);
 
 // ROUTES
-app.get('/', async (req, res) => {
-    const photos = await Photo.find({}).sort('-date');
-    res.render('index', {
-        photos,
-    });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/photos/:id', async (req, res) => {
-    const photo = await Photo.findById(req.params.id);
-    res.render('photo', {
-        photo,
-    });
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/add', (req, res) => {
-    res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-    const uploadsDir = './public/uploads';
-
-    if (!fs.existsSync(uploadsDir)) {
-        fs.mkdirSync(uploadsDir);
-    }
-
-    let uploadedImage = req.files.image;
-    let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
-
-    uploadedImage.mv(uploadPath, async () => {
-        await Photo.create({
-            ...req.body,
-            image: '/uploads/' + uploadedImage.name,
-        });
-
-        res.redirect('/');
-    });
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 // port
 const PORT = 3000;
