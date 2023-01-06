a7c4c0baa9b3836a8295da2683e74c3415378503
Onur Alp <onuralpkayag@gmail.com>
2023-01-06 17:05:11 +0300

    pagination done

--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -3,9 +3,19 @@ const fs = require('fs');
 const Photo = require('../models/Photo');
 
 exports.getAllPhotos = async (req, res) => {
-    const photos = await Photo.find({}).sort('-date');
+    const page = req.query.page || 1;
+    const photosPerPage = 3;
+    const totalPhotos = await Photo.find().countDocuments();
+
+    const photos = await Photo.find({})
+        .sort('-date')
+        .skip((page - 1) * photosPerPage)
+        .limit(photosPerPage);
+
     res.render('index', {
-        photos,
+        photos: photos,
+        current: page,
+        pages: Math.ceil(totalPhotos / photosPerPage),
     });
 };
 
